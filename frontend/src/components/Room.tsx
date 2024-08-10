ba9946258bca4a6f1731d7d379d1925b4dd6d4af
yashraj02 <yashkhokarale@gmail.com>
2024-08-10 11:01:34 +0530

    logic for accepting new user

---
+++ frontend/src/components/Room.tsx
@@ -0,0 +1,14 @@
+import {useEffect} from 'react';
+import {useSearchParams} from 'react-router-dom';
+
+export const Room = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const name = searchParams.get('name');
+
+  useEffect(() => {
+    // logic to init user to the room
+  });
+
+  return <div> Hi {name} </div>;
+};
